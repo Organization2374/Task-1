0fc174223bb55bc34477bdcbe6cfd4c16e53b342
Yurov Pavel <sendmeof@gmail.com>
2024-08-02 18:12:59 +0300

    JDBC Realization

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/users";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "P@ssw0rd";
+
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
