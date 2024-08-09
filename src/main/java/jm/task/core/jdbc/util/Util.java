521d3610531fa2878bde0c306119b63d41a3ad76
Yurov Pavel <sendmeof@gmail.com>
2024-08-09 20:32:19 +0300

    Hibernate realization Логер не используем т.к. внедрен параметр: SHOW_SQL, "true"

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,8 +1,16 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
 
@@ -21,5 +29,34 @@ public class Util {
             e.printStackTrace();
         }
         return connection;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        SessionFactory sessionFactory = null;
+        try {
+
+            Configuration configuration = new Configuration();
+            Properties properties = new Properties();
+            properties.put(Environment.DRIVER, "org.postgresql.Driver");
+            properties.put(Environment.URL, URL);
+            properties.put(Environment.USER, USER);
+            properties.put(Environment.PASS, PASSWORD);
+            properties.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
+            properties.put(Environment.SHOW_SQL, "true");
+            properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+//                properties.put(Environment.HBM2DDL_AUTO, "");
+
+            configuration.setProperties(properties);
+            configuration.addAnnotatedClass(User.class);
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties())
+                    .build();
+
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sessionFactory;
     }
 }
