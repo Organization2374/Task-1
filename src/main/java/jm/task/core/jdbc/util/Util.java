5e294b5c5c23f499e4500ce79ea5414b0e604fa3
Yurov Pavel <sendmeof@gmail.com>
2024-08-13 21:43:50 +0300

    Fix: исправление замечаний

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,6 +1,7 @@
 package jm.task.core.jdbc.util;
 
 import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
@@ -44,7 +45,6 @@ public class Util {
             properties.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
             properties.put(Environment.SHOW_SQL, "true");
             properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-//                properties.put(Environment.HBM2DDL_AUTO, "");
 
             configuration.setProperties(properties);
             configuration.addAnnotatedClass(User.class);
@@ -54,7 +54,7 @@ public class Util {
                     .build();
 
             sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-        } catch (Exception e) {
+        } catch (HibernateException e) {
             e.printStackTrace();
         }
         return sessionFactory;
